c0bbb80e7eb15ccb26d2f6a5ed9ac7bf3677c3b3
Bryan1150 <bryan.umaa1150@gmail.com>
2021-09-09 01:12:22 -0600

    Lab 4 terminado

---
+++ Laboratorio4/Laboratorio4/Controllers/PlanetasController.cs
@@ -0,0 +1,89 @@
+﻿using Laboratorio4.Handlers;
+using Laboratorio4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Laboratorio4.Controllers
+{
+    public class PlanetasController : Controller
+    {
+        // GET: Planetas
+      public ActionResult listadoDePlanetas()
+        {
+            PlanetasHandler accesoDatos = new PlanetasHandler();
+            ViewBag.planetas = accesoDatos.obtenerTodoslosPlanetas();
+            return View();
+        }
+            public ActionResult crearPlaneta()
+        {
+            return View();
+        }
+
+            [HttpPost]
+        public ActionResult crearPlaneta(PlanetaModel planeta)
+        {
+            ViewBag.ExitoAlCrear = false;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    PlanetasHandler accesoDatos = new PlanetasHandler();
+                    ViewBag.ExitoAlCrear = accesoDatos.crearPlaneta(planeta);
+                    if (ViewBag.ExitoAlCrear)
+                    {
+                        ViewBag.Message = "El planeta " + planeta.nombre + "fue creado con exito! owo";
+                        ModelState.Clear();
+                    }
+                }
+                return View();
+            }
+            catch
+            {
+                ViewBag.Message = "Algo salio mal y no fue posible crear el planeta unu";
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult editarPlaneta(int ? identificador)
+        {
+            ActionResult vista;
+            try
+            {
+                PlanetasHandler accesoDatos = new PlanetasHandler();
+                PlanetaModel planetaModificar = accesoDatos.obtenerTodoslosPlanetas().Find(smodel => smodel.id == identificador);
+                if(planetaModificar == null)
+                {
+                    vista = RedirectToAction("listadoDePlanetas");
+                }
+                else
+                {
+                    vista = View(planetaModificar);
+                }
+            }
+            catch
+            {
+                vista = RedirectToAction("listadoDePlanetas");
+            }
+            return vista;
+        }
+
+        [HttpPost]
+        public ActionResult editarPlaneta(PlanetaModel planeta)
+        {
+            try
+            {
+                PlanetasHandler accesoDatos = new PlanetasHandler();
+                accesoDatos.modificarPlaneta(planeta);
+                return RedirectToAction("Index", "Home");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Laboratorio4/SQLQuery1.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE Planeta(
+	planetaId INTEGER PRIMARY KEY IDENTITY, --identity es para que se incremente automaticamente 1,2,3...
+	nombrePlaneta NVARCHAR(MAX) NOT NULL,
+	numeroAnillos INTEGER NOT NULL,
+	tipoPlaneta NVARCHAR(100) NOT NULL,
+	archivoPlaneta VARBINARY(MAX),
+	tipoArchivo NVARCHAR(50) --esto es solo para guardar la extension del archivo png, pdf
+
+)
